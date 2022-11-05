b5e2bd6846a69a0583f7f087bd9ba2704adfac83
Veronika <vkotsubiak@gmail.com>
2022-11-05 16:42:28 +0200

    3

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from bs4 import BeautifulSoup
 import requests
+
 response = requests.get("https://coinmarketcap.com/")
 
-response_text = response.text
-response_span = response_text.split('<span>')
-for parse_elem_1 in response_span:
-    if parse_elem_1.startswith("$"):
-        for parse_elem_2 in parse_elem_1.split('/span>'):
-            if parse_elem_2.startswith('$') and parse_elem_2[1].isdigit():
-                print(parse_elem_2)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, features="html.parser")
+    soup_list = soup.find_all(
+        'a',
+        {
+            'href': '/currencies/bitcoin/markets/'
+        }
+    )
+    res = soup_list[0].find('span')
+    print(res.text)
